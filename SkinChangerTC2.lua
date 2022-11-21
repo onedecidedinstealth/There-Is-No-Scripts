42a0b5853de0ad8c7827142b2226a506cd382ae4
ren <106044611+onedecidedinstealth@users.noreply.github.com>
2022-11-21 06:33:51 -0700

    Update SkinChangerTC2.lua

--- SkinChangerTC2.lua
+++ SkinChangerTC2.lua
@@ -1,13 +1,14 @@
 local SkinChanger = {}
+local Viewmodels = game.ReplicatedStorage.Viewmodels
 function SkinChanger:ChangeViewmodels(vwm1,vwm2)
 local wep,wep2,name,name2
-   for i,v in pairs(vwm:GetChildren()) do
+   for i,v in pairs(Viewmodels:GetChildren()) do
       if v.Name == vwm1 and not v.Name:match("RLV") then
          name = v.Name
          wep = v
       end
    end
-   for i2,v2 in pairs(vwm:GetChildren()) do
+   for i2,v2 in pairs(Viewmodels:GetChildren()) do
       if v2.Name == vwm2 and not v2.Name:match("RLV") then
          name2 = v2.Name
          wep2 = v2
@@ -20,7 +21,7 @@ local wep,wep2,name,name2
 end
 
 function SkinChanger:RevertViewmodels()
-   for i,v in pairs(vwm:GetChildren()) do
+   for i,v in pairs(Viewmodels:GetChildren()) do
       if v:GetAttribute("ViewmodelChanged") then
          v.Name = tostring(v:GetAttribute("ViewmodelChanged"))
       end
